28afb6ba80da703b97e75ac2f31255cb42c08f83
Timo Kaipiainen <timo.kaipiainen@gmail.com>
2014-06-24 22:18:32 +0300

    quick and dirty report figures

--- src/JYPS/RegisterBundle/Controller/ReportController.php
+++ src/JYPS/RegisterBundle/Controller/ReportController.php
@@ -21,9 +21,43 @@ class ReportController extends Controller
     		->where('m.membership_end_date >= :current_date')
     		->setParameter('current_date', new \DateTime("now") )
     		->getQuery();
-  
+  		$total_members = 0;
+  		$male_qty = 0;
+  		$female_qty = 0;
+  		$female_juniors = 0;
+  		$male_juniors = 0;
+  		$e_magazine = 0;
+  		$join_year_qty = [];
+  		$zip_codes = [];
+  		$membership_types = [];
   		$members = $query->getResult();
+  		foreach($members as $member) {
+	  		$total_members++;
+	  		if ($member->getGender() == False) {
+	  			$female_qty++;
+	  		}
+	  		else {
+		  		$male_qty++;
+	  		}
+	  		if($member->getMagazinePreference() == True) {
+	  			$e_magazine++;
+	  		}
+	  		if(date('Y') - $member->getBirthYear() < 18) {
+	  			if ($member->getGender() == False) {
+	  				$male_juniors++;
+	  			}
+	  			else {
+	  				$female_juniors++;
+	  			}
+	  		}
 
-  		
+	  	}
+	return $this->render('JYPSRegisterBundle:Member:reports.html.twig', 
+		array('total_members' => $total_members,
+    		  'male_qty' => $male_qty, 
+    		  'female_qty' => $female_qty,
+    		  'e_magazine' => $e_magazine,
+    		  'male_juniors' => $male_juniors,
+    		  'female_juniors' => $female_juniors));
 	}
 }

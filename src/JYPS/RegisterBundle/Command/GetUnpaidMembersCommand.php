f9d61d539c106b0c722a8eba9634802631dddc8a
Timo Kaipiainen <timo.kaipiainen@gmail.com>
2015-11-17 21:41:15 +0200

    new console command

---
+++ src/JYPS/RegisterBundle/Command/GetUnpaidMembersCommand.php
@@ -0,0 +1,44 @@
+<?php
+// src/JYPS/RegisterBundle/Command/GetUnpaidMembersCommand.php
+
+namespace JYPS\RegisterBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetUnpaidMembersCommand extends ContainerAwareCommand {
+	protected function configure() {
+		$this
+			->setName('get_unpaid')
+			->setDescription('Print unpaid members for given period')
+			->addOption('period', null, InputOption::VALUE_NONE, 'Fee period that is checked');
+	}
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$period = $input->getOption('period');
+		$em = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+		$repository = $this->getContainer()->get('doctrine')
+			->getRepository('JYPSRegisterBundle:Member');
+
+		$query = $repository->createQueryBuilder('m')
+			->where('m.membership_end_date >= :current_date')
+			->setParameter('current_date', new \DateTime("now"))
+			->getQuery();
+		$period = 2015;
+		$members = $query->getResult();
+
+		foreach ($members as $member) {
+
+			foreach ($member->getMemberFees() as $memberfee) {
+
+				if ($memberfee->getFeePeriod() == $period && $memberfee->getPaid() == 0) {
+					echo $member->getMemberId() . ";" . $member->getFirstName() . " " . $member->getSurname() . "\n";
+				}
+
+			}
+		}
+	}
+
+}

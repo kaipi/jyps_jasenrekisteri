f1a0017f190f4d233605872b84f8a5a25aee36d2
Timo Kaipiainen <timo.kaipiainen@gmail.com>
2015-11-17 21:56:12 +0200

    only paper

--- src/JYPS/RegisterBundle/Command/GetUnpaidMembersCommand.php
+++ src/JYPS/RegisterBundle/Command/GetUnpaidMembersCommand.php
@@ -23,8 +23,9 @@ class GetUnpaidMembersCommand extends ContainerAwareCommand {
 			->getRepository('JYPSRegisterBundle:Member');
 
 		$query = $repository->createQueryBuilder('m')
-			->where('m.membership_end_date >= :current_date')
+			->where('m.membership_end_date >= :current_date AND m.magazine_preference = :magazine_pref')
 			->setParameter('current_date', new \DateTime("now"))
+			->setParameter('magazine_pref', 0)
 			->getQuery();
 		$period = 2015;
 		$members = $query->getResult();

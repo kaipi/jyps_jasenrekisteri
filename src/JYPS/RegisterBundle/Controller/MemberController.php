bb61a57455c7562f980d1b5dbb2d30a199614a1d
Timo Kaipiainen <timo.kaipiainen@gmail.com>
2016-01-30 19:23:27 +0200

    changes to templates and emails

--- src/JYPS/RegisterBundle/Controller/MemberController.php
+++ src/JYPS/RegisterBundle/Controller/MemberController.php
@@ -184,7 +184,7 @@ class MemberController extends Controller {
 		$message = \Swift_Message::newInstance()
 			->setSubject('Uusi JYPS-jäsen!')
 			->setFrom('rekisteri@jyps.fi')
-			->setTo(array('teemu.tenhunen@gmail.com', 'jyrki.solja@gmail.com'))
+			->setTo(array('teemu.j.tenhunen@gmail.com', 'jyrki.solja@gmail.com'))
 			->setBody($this->renderView(
 				'JYPSRegisterBundle:Member:join_member_infomail.txt.twig',
 				array('member' => $member,

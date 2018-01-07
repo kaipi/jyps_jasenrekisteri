87b223f7bcb2566053b51e22e56642442794025c
Timo Kaipiainen <timo.kaipiainen@sc5.io>
2018-01-07 18:08:03 +0200

    prevent production deployment for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ case $1 in
     ssh git@jyps.fi "php /usr/share/nginx/betarekkari.jyps.fi/app/console cache:clear --env=dev"
     ;;
   production)
-    rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ git@jyps.fi:/us/deplotesti
-    #ssh git@jyps.fi "php /usr/share/nginx/betarekkari.jyps.fi/app/console cache:clear --env=production"
+    #rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ git@jyps.fi:/usr/share/nginx/jasenrekisteri.jyps.fi
+    #ssh git@jyps.fi "php /usr/share/nginx/jasenrekisteri.jyps.fi/app/console cache:clear --env=production"
     ;;
 esac

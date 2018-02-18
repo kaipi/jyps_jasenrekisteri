307a54b048a5d60be89d9eeafc8bcc578ba001c6
Timo Kaipiainen <timo.kaipiainen@sc5.io>
2018-02-18 20:05:52 +0200

    added excludes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $1 in
   development)
-    rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ git@jyps.fi:/usr/share/nginx/betarekkari.jyps.fi
+    rsync -r --delete-after --exclude-from 'scripts/exclude.txt' --quiet $TRAVIS_BUILD_DIR/ git@jyps.fi:/usr/share/nginx/betarekkari.jyps.fi
     ssh git@jyps.fi "php /usr/share/nginx/betarekkari.jyps.fi/app/console cache:clear --env=dev"
     ;;
   production)
